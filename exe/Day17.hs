cee30ca8d816c2c666cde987d7617ebc20d5fcd9
Paul Ogris <paul@tsahyt.com>
2023-12-17 10:41:24 +0100

    Day 17 Part 2
    Correct but slow

--- exe/Day17.hs
+++ exe/Day17.hs
@@ -7,12 +7,12 @@ module Main where
 import Algorithm.Search
 import Data.Map (Map)
 import Data.Map qualified as M
-import Data.List (group)
 
 main :: IO ()
 main = do
   input <- parse <$> readFile "inputs/day17"
-  print $ part1 input
+  -- print $ part1 input
+  print $ part2 input
 
 type Pos = (Int, Int)
 
@@ -31,11 +31,8 @@ parse =
   concatMap (\(i, s) -> map (\(j, c) -> ((i, j), read . pure $ c)) . zip [0..] $ s) .
   zip [0..] . lines
 
-manhattan :: Pos -> Pos -> Int
-manhattan (ax, ay) (bx, by) = abs (bx - ax) + abs (by - ay)
-
-shortestPath :: Map Pos Int -> Pos -> Pos -> Maybe _
-shortestPath m from to = dijkstra neighbors cost ((== to) . fst) (from, [])
+defaultRules :: Map Pos Int -> Pos -> Pos -> Maybe _
+defaultRules m from to = dijkstra neighbors cost ((== to) . fst) (from, [])
   where
     neighbors ((i, j), hist) = filter (uncurry constraints)
       [ ((i - 1, j), North : take 3 hist), ((i + 1, j), South : take 3 hist)
@@ -46,17 +43,28 @@ shortestPath m from to = dijkstra neighbors cost ((== to) . fst) (from, [])
       && (length hist > 2 ==> hist !! 0 /= opposite (hist !! 1))
     cost _ (pos, _) = m M.! pos
 
+ultraRules :: Map Pos Int -> Pos -> Pos -> Maybe _
+ultraRules m from to = dijkstra neighbors cost ((== to) . fst) (from, [])
+  where
+    neighbors ((i, j), hist) = filter (uncurry constraints)
+      [ ((i - 1, j), North : take 10 hist), ((i + 1, j), South : take 10 hist)
+      , ((i, j - 1), West : take 10 hist), ((i, j + 1), East : take 10 hist) ]
+    constraints pos hist =
+      pos `M.member` m
+      && (length hist == 11 ==> not (allEqual hist))
+      && (length hist > 1 ==> (hist !! 0 /= hist !! 1 ==> allEqual (take 4 . drop 1 $ hist)))
+      && (length hist > 2 ==> hist !! 0 /= opposite (hist !! 1))
+    cost _ (pos, _) = m M.! pos
+
 infixl 3 ==>
 (==>) :: Bool -> Bool -> Bool
 a ==> b = not a || b
-
-minEqual :: Eq a => Int -> [a] -> Bool
-minEqual l xs
-  | length xs <= l = True
-  | otherwise = (>= l) . length . head . group $ xs
 
 allEqual :: Eq a => [a] -> Bool
 allEqual xs = and $ map (== head xs) (tail xs)
 
 part1 :: Map Pos Int -> Maybe Int
-part1 m = fmap fst $ shortestPath m (0,0) (maximum . M.keys $ m)
+part1 m = fmap fst $ defaultRules m (0,0) (maximum . M.keys $ m)
+
+part2 :: Map Pos Int -> Maybe Int
+part2 m = fmap fst $ ultraRules m (0,0) (maximum . M.keys $ m)
